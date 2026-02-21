be0c5b1fd87e1e0c9ca3f8c5107f3a6159a8a461
analu0505 <luciacarvajalm05@gmail.com>
2026-02-21 17:57:37 -0600

    Final de clase 3

---
+++ Utils/DbHelper.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Data.SqlClient
+
+Public Class DbHelper
+    Private ReadOnly connectionString As String = ConfigurationManager.ConnectionStrings("II-46_PROGRAIIIConnectionString").ConnectionString
+
+    Public Function Getconexion() As SqlConnection
+        Return New SqlConnection(connectionString)
+
+    End Function
+
+    Public Function CrearPersona() As Integer
+        Dim Sql As String = "INSERT INTO Personas(Nombre, Apellidos, Fecha_Nacimiento, Correo, Numero_Documento, Tipo_Documento )
+        VALUES( @Nombre, @Apellidos, @Fecha_Nacimiento, @Correo, @Numero_Documento, @Tipo_Documento)"
+        'CONEXION A LA BASE DE DATOS
+        Using conn As SqlConnection = Getconexion()
+            Using cmd As New SqlCommand(Sql, conn)
+                conn.Open()
+                Return cmd.ExecuteNonQuery()
+            End Using
+        End Using
+        Return 0
+    End Function
+End Class
